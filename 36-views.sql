ab7cea925b0f2087b193c9de902f6214f115f71f
Dipanshu-26 <dnchawde@gmail.com>
2026-06-15 20:18:29 +0530

    15-June

--- 36-views.sql
+++ 36-views.sql
@@ -25,6 +25,69 @@ joining_date, experience_years,salary
 from employees_training;
 
 select * from emp_basic_cleaning;
+-- ------------------------------------------------------------------------
+-- view 2 : identify bad data 
+
+select emp_id, emp_name, department, experience_years, salary
+from employees_training;
+
+-- view 
+
+create view vw_employee_position as
+select emp_id, emp_name,
+ifnull(department,"not assigned") as department,
+case 
+when experience_years < 6 then "lead"
+else "manager"
+end as position
+from employees_training;
+
+select * from vw_employee_position;
+
+-- ------------------------------------------------------------
+
+create view employee_vw as 
+select emp_id, emp_name, salary from employees_training;
+
+select * from employee_vw;
+
+select emp_name, salary from employee_vw where salary > 90000;
+
+-- ----------------------------------------------------------
+
+-- update 
+update employee_vw
+set salary = 95000
+where emp_id = 102;
+
+-- Non-Updatable View ❌
+
+-- If the view contains:
+
+-- GROUP BY
+-- DISTINCT
+-- Aggregate functions (SUM, AVG, COUNT)
+-- UNION
+-- Window functions
+-- Multiple complex joins
+
+-- then MySQL will not allow updates.
+-- --------------------------------------------------------------
+
+create view dept_summery_vw as
+select department, 
+avg(salary) as avg_sal
+from employees_training
+group by department;
+
+select * from dept_summery_vw;
+
+-- update : not possible in above viwe
+update dept_summery_vw
+set avg_salary = 100000
+where department = "sales";
+
+
 
 
 
